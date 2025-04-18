aee58c59a18e455f7ff731e019179468b6d473f0
Fabrício Landskren Folmer <fabriciolfolmer@gmail.com>
2025-04-18 13:32:07 -0300

    Commit Inicial

---
+++ config_manager.py
@@ -0,0 +1,39 @@
+import json, os
+
+# Classe ConfigManager para gerenciar a configuração do jogo
+# Se o arquivo de configuração não existir, ele será criado com valores padrão
+# Se o arquivo existir, ele será carregado e os valores serão atualizados
+class ConfigManager:
+    default_config = {
+        "window_width": 1600,
+        "window_height": 900,
+        "title": "Flappy GLFW",
+        "fps": 60
+    }
+
+    def __init__(self, filepath="config.json"):
+        self.filepath = filepath
+        self.config = self.default_config.copy()
+        self.load()
+
+    def load(self):
+        if os.path.exists(self.filepath):
+            with open(self.filepath, "r") as f:
+                try:
+                    data = json.load(f)
+                    self.config.update(data)
+                except json.JSONDecodeError:
+                    print("⚠️ Invalid JSON format. Using defaults.")
+     #   else:
+     #       self.save()
+
+    def save(self):
+        with open(self.filepath, "w") as f:
+            json.dump(self.config, f, indent=4)
+
+    def get(self, key, default=None):
+        return self.config.get(key, default)
+
+    def set(self, key, value):
+        self.config[key] = value
+        self.save()
